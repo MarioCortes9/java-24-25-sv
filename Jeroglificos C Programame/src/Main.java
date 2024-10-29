24edd18feff1f3363d496ff9a547af7c396bce11
MarioCortes9 <duquegarciap@gmail.com>
2024-10-29 18:42:11 +0100

    Update Main.java

--- Jeroglificos C Programame/src/Main.java
+++ Jeroglificos C Programame/src/Main.java
@@ -20,25 +20,27 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner lector = new Scanner(System.in);
-    private static int [] potencia= {1000000,100000,10000,1000,100,10,1};
-    private static char [] simbolo={'H','R','D','F','C','G','T'};
+    private static int [] potencia= {1000000,100000,10000,1000,100,10,1}; //POTENCIAS
+    private static char [] simbolo={'H','R','D','F','C','G','T'};//SIMBOLOS DE LAS PALABRAS
 
-
+    //PRIMERO CREAMOS UNA FUNCION DONDE SE VAYAN LEYENDO LOS NUMEROS INGRESADOS.
     private static int leeNumero(){
         System.out.println("Introduce un numero del (1 a 9.999.999) o 0 si desea salir del programa");
         return lector.nextInt();
     }
-
+    // FUNCION PARA LA CONVERSION DE NUMERO A EGIPCIO
     private static String aNumeroEgipcio(int numero){
-
-
+        //CADENA QUE GUARDE LOS RESULTADOS
         String resultado=" ";
-
+        
         for (int i = 0; i < potencia.length; i++) {
+            // CANTIDAD
            int cantidad= numero / potencia[i];
+            //VALOR DEL NUMERO
            numero = numero % potencia[i];
 
             for (int j = 0; j < cantidad; j++) {
+                //VALOR RESULTADO
                 resultado = resultado + simbolo[i];
             }
         }
@@ -46,17 +48,19 @@ public class Main {
     }
 
     private static void imprimirResultado(String resultado){
+        //SOLCUION EN NOTACION EGIPCIA
         System.out.println("Resultado en notación egipcia :" + resultado);
     }
 
     public static void main(String[] args) {
-
+        //SI NUMERO EN EL INTERVALO
         while (true){
             int numero=leeNumero();
+            //SI NUMERO ES 0 VUELVE AL INICIO
             if(numero==0){
                 break;
             }
-
+            //GUARDE EN LA CADENA EL RESULTADO DE LA CONVERSION
             String resultado=aNumeroEgipcio(numero);
             imprimirResultado(resultado);
         }
